b75cf390cd2634cbf5a681c4f49dc88c96efa802
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2024-06-19 16:12:33 +0200

    Add GitHub Action
    * https://nix.dev/tutorials/nixos/continuous-integration-github-actions.html
    * https://github.com/DeterminateSystems/magic-nix-cache-action

--- flake.nix
+++ flake.nix
@@ -14,26 +14,31 @@
       flake-utils,
       ...
     }:
-    flake-utils.lib.eachSystem [ "aarch64-darwin" ] (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages.default = home-manager.defaultPackage.${system};
+    flake-utils.lib.eachSystem
+      [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ]
+      (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          packages.default = home-manager.defaultPackage.${system};
 
-        packages.homeConfigurations.mngrm3a = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [ ./home.nix ];
-          extraSpecialArgs = { };
-        };
+          packages.homeConfigurations.mngrm3a = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [ ./home.nix ];
+            extraSpecialArgs = { };
+          };
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nil
-            pkgs.nixfmt-rfc-style
-          ];
-        };
-      }
-    );
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nil
+              pkgs.nixfmt-rfc-style
+            ];
+          };
+        }
+      );
 }
