67099e3e84f0812554ec0d16ca0b818785ee7147
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2024-06-27 16:07:42 +0200

    Simplify tmux config, remove gitmux

--- home-manager/mngrm3a/programs/tmux.nix
+++ home-manager/mngrm3a/programs/tmux.nix
@@ -23,18 +23,7 @@
       }
       { plugin = tmux-fzf; }
     ];
-    extraConfig = ''
-      set-window-option -g automatic-rename on
-      set-option -g set-titles on
-      setw -g mouse on
-      setw -g monitor-activity on
-      set -g status-right "#[bg=colour237,fg=colour239 nobold, nounderscore, noitalics]#[bg=colour239,fg=colour246] #(gitmux \"#{pane_current_path}\")#[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]  %Y-%m-%d  %H:%M #[bg=colour239,fg=colour248,nobold,noitalics,nounderscore]#[bg=colour248,fg=colour237] #h"
-    '';
-    # handled by sensible
-    # https://nix-community.github.io/home-manager/options.html#opt-programs.tmux.sensibleOnTop
-    # https://github.com/tmux-plugins/tmux-sensible#options
+    extraConfig = builtins.readFile ./tmux.conf;
     terminal = "screen-256color";
   };
-
-  home.packages = [ pkgs.gitmux ];
 }
