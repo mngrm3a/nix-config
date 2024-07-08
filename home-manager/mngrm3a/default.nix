08cba8ed05ab629b3ba135fefb44e2021d8c2d47
mngrm3a <9266859+mngrm3a@users.noreply.github.com>
2024-07-08 19:18:19 +0200

    Add stacks config.yaml

--- home-manager/mngrm3a/default.nix
+++ home-manager/mngrm3a/default.nix
@@ -38,14 +38,6 @@ in
   ];
   programs.home-manager.enable = true;
 
-  home.file."Library/LaunchAgents/com.local.KeyRemapping.plist" = {
-    # NOTE:
-    # * CapsLock -> LeftControl
-    # * RightOption -> LeftControl
-    enable = isDarwin;
-    source = ./macos/com.local.KeyRemapping.plist;
-  };
-
   programs.ripgrep.enable = true;
   programs.zoxide.enable = true;
 
@@ -55,4 +47,24 @@ in
       theme = "gruvbox-dark";
     };
   };
+
+  home.file."Library/LaunchAgents/com.local.KeyRemapping.plist" = {
+    # NOTE:
+    # * CapsLock -> LeftControl
+    # * RightOption -> LeftControl
+    enable = isDarwin;
+    source = ./macos/com.local.KeyRemapping.plist;
+  };
+
+  home.file.".stack/config.yaml".text = lib.generators.toYAML { } {
+    templates = {
+      params = {
+        author-name = "mngrm3a";
+        author-email = "9266859+mngrm3a@users.noreply.github.com";
+        copyright = "2024 mngrm3a";
+        github-username = "mngrm3a";
+      };
+    };
+  };
+
 }
